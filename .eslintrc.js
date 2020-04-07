852ee641059e1b7778e4dda82fd10657b7cb3f34
CI-Walter <walter.huang@cloud-interactive.com>
2020-04-07 14:39:24 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  plugins: ['vue', 'prettier'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'handle-callback-err': 'off',
+    'no-template-curly-in-string': 'off',
+    camelcase: 'off',
+    'prettier/prettier': 'error'
+  }
+}
